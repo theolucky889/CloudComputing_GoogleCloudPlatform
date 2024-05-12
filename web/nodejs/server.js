2ab368da8bfdf0d6c9cb870cc91e812e018e54f5
theolucky889 <theolucky889@gmail.com>
2024-05-13 00:51:03 +0800

    Update server.js

--- web/nodejs/server.js
+++ web/nodejs/server.js
@@ -1,2 +1,22 @@
 const express = require('express');
-const multer = require('multer')
+const multer = require('multer');
+const { ImageAnnotatorClient } = require('@google-cloud/vision');
+const app = express();
+const port = 3000;
+
+const client = new ImageAnnotatorClient({
+    keyFilename: ''
+});
+const upload = multer({dest: 'uploads/'})
+
+app.use(express.static('public'))   //serve static file
+
+app.post('/analyze', upload.single('image'), async (req, res) => {
+    const [result] = await client.documentTextDetection(`./uploads/${req.file.filename}`);
+    const detections = result.textAnnotations;
+    res.json({ text: detections[0] ? detections[0].description : "No text detected." });
+});
+
+app.listen(port, () => {
+    console.log('Server running on http://localhost:${port}');
+});
