1a3c06296609841f5ad37d64aef660f1f4c15106
theolucky889 <theolucky889@gmail.com>
2024-05-13 00:42:52 +0800

    Create app.js

---
+++ web/app.js
@@ -0,0 +1,16 @@
+document.getElementById('imageForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    const imageFile = document.getElementById('iamgeUpload').files[0];
+    formData.append('image', imageFile)
+
+    fetcj('/analyze', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('results').innerHTML = '<p>Detected Text: ${data.text}</p>';
+    })
+    .catch(err => console.error('Error', err));
+});
